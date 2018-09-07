092e0a266e6d8d79d37fa300d557396d54aec74a
SYProkopchuk <prokopchuk.serg@mail.ru>
2018-09-07 21:25:30 +0400

    Add lost comparison operators

--- Sum of positive.java
+++ Sum of positive.java
@@ -2,12 +2,12 @@ public class Positive{
 
   public static int sum(int[] arr){
   int a=0;
-   for (int i=0;iarr.length;i++){
-     if (arr[i]0){
+   for (int i=0;i<arr.length;i++){
+     if (arr[i]>0){
      a += arr[i];
      } 
    }
-   if (a0) {
+   if (a<0) {
    return 0;
    } else return a; 
   }
